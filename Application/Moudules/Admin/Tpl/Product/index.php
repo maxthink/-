67bb1fc0bbd49ade731082c611fea42e23ec0fca
maxthink <maxthink@live.com>
2018-07-17 23:22:12 +0800

    页面一般性修改

--- Application/Moudules/Admin/Tpl/Product/index.php
+++ Application/Moudules/Admin/Tpl/Product/index.php
@@ -27,35 +27,61 @@ var CLICK_EDIT_CONTENT = '点击修改内容';
 </script>
 </head>
 <body>
-	<div class="cw-body">
-		<div class="fb-title"><div><p><span>产品 > 列表 </span></p></div></div>
-		<div class="fb-body">
-			<table class="body-table" cellpadding="0" cellspacing="1" border="0">
-				<tr>
-					<td class="body-table-td">
-						<div class="body-table-div">
-							<script type="text/javascript" src="/admin/Tpl/Default/Static/Js/dataList.js"></script>
-							<div class="handle-btns">
-								<div class="img-button "><p><input type="button" id="addAdmin" name="addAdmin" value="添加" onclick="addData()" class="addAdmin"></p></div>
-								<div class="img-button "><p><input type="button" id="editAdmin" name="editAdmin" value="编辑" onclick="editData(this,'checkList','uid')" class="editAdmin"></p></div>
-								
-							</div>
-							<html:list
-							id="checkList"
-							name="user"
-							style="table-list"
-							action="true"
-							datasource="list"
-							show="id:编号,title:名称:editData,intro:简介,time:时间,status|getStatus:状态"
-							actionlist="editData:编辑,showData:详情" />
-							<div class="pager">{$page}</div>
-						</div>
-					</td>
-				</tr>
-			</table>
-		</div>
-	</div>
-	<div class="ajax-loading"></div>
+    <div class="cw-body">
+        <div class="fb-title"><div><p><span>产品 > 列表 </span></p></div></div>
+        <div class="fb-body">
+            <table class="body-table" cellpadding="0" cellspacing="1" border="0">
+                <tr>
+                    <td class="body-table-td">
+                        <div class="body-table-div">
+                            <script type="text/javascript" src="/admin/Tpl/Default/Static/Js/dataList.js"></script>
+                            <div class="handle-btns">
+                                    <div class="img-button "><p><input type="button" id="addAdmin" name="addAdmin" value="添加" onclick="addData()" class="addAdmin"></p></div>
+                                    <div class="img-button "><p><input type="button" id="editAdmin" name="editAdmin" value="编辑" onclick="editData(this,'checkList','uid')" class="editAdmin"></p></div>
+
+                            </div>
+                            <table id="checkList" class="table-list" cellpadding="0" cellspacing="0">
+                            <tbody>
+                                <tr>
+                                    <td height="5" colspan="8" class="topTd"></td>
+                                </tr>
+                                <tr class="row">
+                                    <th>ID</th>
+                                    <th>名称</th>
+                                    <th>描述</th>
+                                    <th>发布时间</th>
+                                    <th>是否可用</th>
+                                    <th>操作</th>
+                                </tr>
+                                <foreach name="list" item="val" >
+                                <tr class="row">
+                                    <td>{$val.id}</td>
+                                    <td>{$val.name}</td>
+
+                                    <td>{$val.intro}</td>
+
+                                    <td>{$val.time|date='Y-m-d H:i',###}</td>
+                                    <td>
+                                        <if condition="$val.status eq 1 ">
+                                            <img src="/Public/admin/images/status-1.gif" border="0" alt="正常" onclick="getStatus()">
+                                        <else />
+                                            <img src="/Public/admin/images/status-0.gif" border="0" alt="正常" onclick="getStatus()">
+                                        </if>
+                                    </td>
+                                    <td><a href="javascript:modifyData('{$val.id}')">编辑</a></td>
+                                </tr>
+                                </foreach>
+                            </tbody>
+                        </table>
+                        <div class="pager">{$page}</div>
+
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="ajax-loading"></div>
 </body>
 <script type="text/javascript">
 jQuery(function($){
